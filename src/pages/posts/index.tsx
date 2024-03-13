b5093bb80247f06c3f0c6a11deac3b51b47e432d
Mochamad Athhar <fauzan.athhar@gmail.com>
2024-03-13 14:13:29 +0700

    post file removal

--- src/pages/posts/index.tsx
+++
@@ -1,8 +0,0 @@
-import { usePostsList } from "@/hooks/queries/posts";
-import React from "react";
-
-export default function Post() {
-  const { data: dataPosts } = usePostsList();
-
-  return <div>index</div>;
-}
