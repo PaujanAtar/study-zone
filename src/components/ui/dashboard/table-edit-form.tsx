9a1a1e09dfc5aaca7234df3584d8002b67de0eee
Mochamad Athhar <fauzan.athhar@gmail.com>
2024-03-21 14:27:38 +0700

    Dashboard form fix attempt 1

--- src/components/ui/dashboard/table-edit-form.tsx
+++ src/components/ui/dashboard/table-edit-form.tsx
@@ -21,19 +21,18 @@ const TableEditForm = ({ item, onSave }: { item: any; onSave: any }) => {
       className="flex flex-col items-center gap-4 p-6"
       onSubmit={handleSubmit}
     >
-      {Array.isArray(editedItem) &&
-        Object.keys(editedItem)?.map((key: any) => (
-          <label key={key} className="mx-10 flex flex-col">
-            <span className="pb-1 font-bold">{key}:</span>
-            <input
-              className="border-2 p-2"
-              type="text"
-              name={key}
-              value={editedItem[key]}
-              onChange={handleChange}
-            />
-          </label>
-        ))}
+      {Object.keys(editedItem)?.map((key: any) => (
+        <label key={key} className="mx-10 flex flex-col">
+          <span className="pb-1 font-bold">{key}:</span>
+          <input
+            className="border-2 p-2"
+            type="text"
+            name={key}
+            value={editedItem[key]}
+            onChange={handleChange}
+          />
+        </label>
+      ))}
       <button
         className="mt-4 w-14 rounded-lg border border-black"
         type="submit"
